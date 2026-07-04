54142b106587004febee5226e2a260ae2a56c16f
massah <arafeh.massah2007@gmail.com>
2026-07-03 21:06:14 -0400

    coord

---
+++ include/ops/coordinate_batch.h
@@ -0,0 +1,12 @@
+#pragma once
+/*
+- image/grid coordinate generator
+- coordinate-value batch abstraction
+- normalized coordinates, ideally [−1,1]
+- uniform random subsampling of coordinates
+*/
+
+class CoordinateBatch {
+public:
+private:
+}
