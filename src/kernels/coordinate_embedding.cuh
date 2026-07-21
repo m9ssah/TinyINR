d3c4482262c0a0d76262e236ef64ed1c01f4b25a
massah <arafeh.massah2007@gmail.com>
2026-07-20 21:03:13 -0400

    cmake updated, started kernel ops modularization

---
+++ src/kernels/coordinate_embedding.cuh
@@ -0,0 +1,19 @@
+#pragma once
+
+__global__ void coordinate_embedding(
+    const float* d_input,
+    float*       d_output,
+    int B,       // batch size
+    int N,       // number of points per batch
+    int D,       // coordinate dimension (e.g. 2 for 2D images)
+    int F        // number of frequency bands
+);
+
+void cpu_coordinate_embedding(
+    const float* input,
+    float* output,
+    int B,
+    int N,
+    int D,
+    int F
+);
