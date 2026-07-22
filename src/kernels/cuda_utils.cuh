2061d4e8431367148e71c2ff3e12fac86c9774d5
massah <arafeh.massah2007@gmail.com>
2026-07-22 14:08:06 -0400

    clang formatting

--- src/kernels/cuda_utils.cuh
+++ src/kernels/cuda_utils.cuh
@@ -5,74 +5,66 @@
 #include <stdlib.h>
 
 // usage example: CUDA_CHECK(cudaMalloc(...));
-#define CUDA_CHECK(call)                                                        \
-    do                                                                          \
-    {                                                                           \
-        cudaError_t _err = (call);                                              \
-        if (_err != cudaSuccess) {                                              \
-            fprintf(stderr, "[CUDA ERROR] %s:%d  %s\n",                         \
-                    __FILE__, __LINE__, cudaGetErrorString(_err));              \
-            exit(EXIT_FAILURE);                                                 \
-        }                                                                       \
-    } while (0)
+#define CUDA_CHECK(call)                                                       \
+  do {                                                                         \
+    cudaError_t _err = (call);                                                 \
+    if (_err != cudaSuccess) {                                                 \
+      fprintf(stderr, "[CUDA ERROR] %s:%d  %s\n", __FILE__, __LINE__,          \
+              cudaGetErrorString(_err));                                       \
+      exit(EXIT_FAILURE);                                                      \
+    }                                                                          \
+  } while (0)
 
 // usage example: CUDA_CHECK_LAST_ERROR()
-#define CUDA_CHECK_LAST_ERROR()                                                 \
-    do                                                                          \
-    {                                                                           \
-        cudaError_t _err = cudaGetLastError();                                  \
-        if (_err != cudaSuccess) {                                              \
-            fprintf(stderr, "[KERNEL LAUNCH ERROR] %s:%d  %s\n",                \
-                    __FILE__, __LINE__, cudaGetErrorString(_err));              \
-            exit(EXIT_FAILURE);                                                 \
-        }                                                                       \
-    } while (0)
-
+#define CUDA_CHECK_LAST_ERROR()                                                \
+  do {                                                                         \
+    cudaError_t _err = cudaGetLastError();                                     \
+    if (_err != cudaSuccess) {                                                 \
+      fprintf(stderr, "[KERNEL LAUNCH ERROR] %s:%d  %s\n", __FILE__, __LINE__, \
+              cudaGetErrorString(_err));                                       \
+      exit(EXIT_FAILURE);                                                      \
+    }                                                                          \
+  } while (0)
 
 // default num, can be overwritten
 #ifndef THREADS_PER_BLOCK
 #define THREADS_PER_BLOCK 256
 #endif
 
-
-inline int compute_grid_size(int total_elements, int threads_per_block = THREADS_PER_BLOCK)
-{
-    return (total_elements + threads_per_block - 1) / threads_per_block;
+inline int compute_grid_size(int total_elements,
+                             int threads_per_block = THREADS_PER_BLOCK) {
+  return (total_elements + threads_per_block - 1) / threads_per_block;
 }
 
-inline float* cuda_alloc(size_t count)
-{
-    float* ptr = nullptr;
-    CUDA_CHECK(cudaMalloc((void**)&ptr, count * sizeof(float)));
-    return ptr;
+inline float *cuda_alloc(size_t count) {
+  float *ptr = nullptr;
+  CUDA_CHECK(cudaMalloc((void **)&ptr, count * sizeof(float)));
+  return ptr;
 }
 
-inline void cuda_h2d(float* d_dst, const float* h_src, size_t count)
-{
-    CUDA_CHECK(cudaMemcpy(d_dst, h_src, count * sizeof(float), cudaMemcpyHostToDevice));
+inline void cuda_h2d(float *d_dst, const float *h_src, size_t count) {
+  CUDA_CHECK(
+      cudaMemcpy(d_dst, h_src, count * sizeof(float), cudaMemcpyHostToDevice));
 }
 
-inline void cuda_d2h(float* h_dst, const float* d_src, size_t count)
-{
-    CUDA_CHECK(cudaMemcpy(h_dst, d_src, count * sizeof(float), cudaMemcpyDeviceToHost));
+inline void cuda_d2h(float *h_dst, const float *d_src, size_t count) {
+  CUDA_CHECK(
+      cudaMemcpy(h_dst, d_src, count * sizeof(float), cudaMemcpyDeviceToHost));
 }
 
-inline bool check_parity(const float* cpu_out, const float* gpu_out,
-                         int count, float tol = 1e-5f)
-{
-    double max_err = 0.0;
-    for (int i = 0; i < count; i++)
-    {
-        double diff = fabs((double)cpu_out[i] - (double)gpu_out[i]);
-        if (diff > max_err) max_err = diff;
-        if (diff > tol)
-        {
-            fprintf(stderr,
-                    "[PARITY FAIL] index %d: cpu=%f  gpu=%f  diff=%e\n",
-                    i, cpu_out[i], gpu_out[i], diff);
-            return false;
-        }
+inline bool check_parity(const float *cpu_out, const float *gpu_out, int count,
+                         float tol = 1e-5f) {
+  double max_err = 0.0;
+  for (int i = 0; i < count; i++) {
+    double diff = fabs((double)cpu_out[i] - (double)gpu_out[i]);
+    if (diff > max_err)
+      max_err = diff;
+    if (diff > tol) {
+      fprintf(stderr, "[PARITY FAIL] index %d: cpu=%f  gpu=%f  diff=%e\n", i,
+              cpu_out[i], gpu_out[i], diff);
+      return false;
     }
-    printf("[PARITY PASS] max absolute error = %e\n", max_err);
-    return true;
+  }
+  printf("[PARITY PASS] max absolute error = %e\n", max_err);
+  return true;
 }
--- tests/test_cpu_cuda_parity.cu
+++ tests/test_cpu_cuda_parity.cu
@@ -4,93 +4,83 @@
 #include <random>
 #include <vector>
 
-#include "../src/kernels/cuda_utils.cuh"
 #include "../src/kernels/coordinate_embedding.cuh"
+#include "../src/kernels/cuda_utils.cuh"
+
 
 struct Case {
-    int B;
-    int N;
-    int D;
-    int F;
+  int B;
+  int N;
+  int D;
+  int F;
 };
 
-static float max_abs_error(const std::vector<float>& a,
-                           const std::vector<float>& b)
-{
-    float max_err = 0.0f;
-    for (size_t i = 0; i < a.size(); ++i) {
-        max_err = std::max(max_err, fabsf(a[i] - b[i]));
-    }
-    return max_err;
+static float max_abs_error(const std::vector<float> &a,
+                           const std::vector<float> &b) {
+  float max_err = 0.0f;
+  for (size_t i = 0; i < a.size(); ++i) {
+    max_err = std::max(max_err, fabsf(a[i] - b[i]));
+  }
+  return max_err;
 }
 
-static void run_case(const Case& c)
-{
-    int input_count = c.B * c.N * c.D;
-    int output_count = c.B * c.N * c.D * c.F * 2;
+static void run_case(const Case &c) {
+  int input_count = c.B * c.N * c.D;
+  int output_count = c.B * c.N * c.D * c.F * 2;
 
-    std::vector<float> input(input_count);
-    std::vector<float> cpu_output(output_count);
-    std::vector<float> gpu_output(output_count);
+  std::vector<float> input(input_count);
+  std::vector<float> cpu_output(output_count);
+  std::vector<float> gpu_output(output_count);
 
-    std::mt19937 rng(123);
-    std::uniform_real_distribution<float> dist(-1.0f, 1.0f);
+  std::mt19937 rng(123);
+  std::uniform_real_distribution<float> dist(-1.0f, 1.0f);
 
-    // init memory
-    for (float& x : input) {
-        x = dist(rng);
-    }
-    
-    // allocate device memory
-    float* d_input = cuda_alloc(input_count);
-    float* d_output = cuda_alloc(output_count);
-    
-    // host to device
-    cuda_h2d(d_input, input.data(), input_count);
+  // init memory
+  for (float &x : input) {
+    x = dist(rng);
+  }
 
-    // launch kernel
-    int threads = THREADS_PER_BLOCK;
-    int blocks = compute_grid_size(input_count, threads);
+  // allocate device memory
+  float *d_input = cuda_alloc(input_count);
+  float *d_output = cuda_alloc(output_count);
 
-    coordinate_embedding<<<blocks, threads>>>(d_input, d_output,
-                                              c.B, c.N, c.D, c.F);
-    CUDA_CHECK_LAST_ERROR();
-    CUDA_CHECK(cudaDeviceSynchronize());
+  // host to device
+  cuda_h2d(d_input, input.data(), input_count);
 
-    // device to host
-    cuda_d2h(gpu_output.data(), d_output, output_count);
+  // launch kernel
+  int threads = THREADS_PER_BLOCK;
+  int blocks = compute_grid_size(input_count, threads);
 
-    // cpu parity check
-    cpu_coordinate_embedding(input.data(), cpu_output.data(),
-                             c.B, c.N, c.D, c.F);
-    float err = max_abs_error(cpu_output, gpu_output);
-    std::cout << "B=" << c.B << " N=" << c.N << " D=" << c.D
-              << " F=" << c.F << " max_abs_error=" << err << "\n";
+  coordinate_embedding<<<blocks, threads>>>(d_input, d_output, c.B, c.N, c.D,
+                                            c.F);
+  CUDA_CHECK_LAST_ERROR();
+  CUDA_CHECK(cudaDeviceSynchronize());
 
-    assert(err < 1e-5f);
+  // device to host
+  cuda_d2h(gpu_output.data(), d_output, output_count);
 
-    // clean up
-    CUDA_CHECK(cudaFree(d_input));
-    CUDA_CHECK(cudaFree(d_output));
+  // cpu parity check
+  cpu_coordinate_embedding(input.data(), cpu_output.data(), c.B, c.N, c.D, c.F);
+  float err = max_abs_error(cpu_output, gpu_output);
+  std::cout << "B=" << c.B << " N=" << c.N << " D=" << c.D << " F=" << c.F
+            << " max_abs_error=" << err << "\n";
+
+  assert(err < 1e-5f);
+
+  // clean up
+  CUDA_CHECK(cudaFree(d_input));
+  CUDA_CHECK(cudaFree(d_output));
 }
 
-int main()
-{
-    std::vector<Case> cases = {
-        {1, 1024, 2, 4},
-        {1, 1024, 2, 8},
-        {1, 1024, 2, 16},
-        {1, 16384, 2, 4},
-        {1, 16384, 2, 8},
-        {1, 16384, 2, 16},
-        {1, 65536, 2, 8},
-        {1, 256, 3, 8}
-    };
+int main() {
+  std::vector<Case> cases = {
+      {1, 1024, 2, 4},  {1, 1024, 2, 8},   {1, 1024, 2, 16}, {1, 16384, 2, 4},
+      {1, 16384, 2, 8}, {1, 16384, 2, 16}, {1, 65536, 2, 8}, {1, 256, 3, 8}};
 
-    for (const Case& c : cases) {
-        run_case(c);
-    }
+  for (const Case &c : cases) {
+    run_case(c);
+  }
 
-    std::cout << "test_cpu_cuda_parity PASS\n";
-    return 0;
+  std::cout << "test_cpu_cuda_parity PASS\n";
+  return 0;
 }
