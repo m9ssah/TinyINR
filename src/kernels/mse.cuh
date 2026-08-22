cba13b84992ee977de61c7a14ae23f484c821b46
massah <arafeh.massah2007@gmail.com>
2026-08-22 12:17:24 -0400

    mse loss added

--- src/kernels/mse.cuh
+++ src/kernels/mse.cuh
@@ -3,4 +3,5 @@
 __global__ void mse_grad(const float *d_output, const float *d_target,
                          float *d_grad, int n);
 
-                         
+__global__ void mse_loss(const float *d_output, const float *d_target,
+                         float *d_loss, int n);
